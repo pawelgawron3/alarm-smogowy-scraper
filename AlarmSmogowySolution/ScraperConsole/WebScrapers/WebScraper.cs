8f4c8ee9d139e429950520eb08c2557be7c061cb
pawelgawron3 <148467604+pawelgawron3@users.noreply.github.com>
2025-07-23 10:54:20 +0200

    Add WebScraper.cs

---
+++ AlarmSmogowySolution/ScraperConsole/WebScrapers/WebScraper.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScraperConsole.WebScrapers;
+public class WebScraper : IDisposable
+{
+    public void Dispose()
+    {
+        throw new NotImplementedException();
+    }
+}
