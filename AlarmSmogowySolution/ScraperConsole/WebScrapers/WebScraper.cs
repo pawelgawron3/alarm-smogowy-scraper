a91adfc8ae67a1ab79e269947b9bb87537e5fe50
pawelgawron3 <148467604+pawelgawron3@users.noreply.github.com>
2025-07-23 13:00:10 +0200

    Update WebScraper.cs
    Implement IDisposable pattern with finalizer-safety

--- AlarmSmogowySolution/ScraperConsole/WebScrapers/WebScraper.cs
+++ AlarmSmogowySolution/ScraperConsole/WebScrapers/WebScraper.cs
@@ -1,14 +1,39 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using OpenQA.Selenium.Chrome;
 
 namespace ScraperConsole.WebScrapers;
 public class WebScraper : IDisposable
 {
+    private ChromeDriver _driver;
+    private bool _disposed = false;
+
+    public WebScraper()
+    {
+        var chromeOptions = new ChromeOptions();
+
+        chromeOptions.AddArguments("--headless"); //without browser GUI
+        chromeOptions.AddArguments("--disable-gpu"); //for stability on Windows/Linux
+        chromeOptions.AddArguments("--disk-cache-size=0"); //always load the new page
+        chromeOptions.AddArguments("--no-sandbox"); //for Linux env
+        chromeOptions.AddArguments("--window-size=1920,1080"); //set the tab resolution
+
+        _driver = new ChromeDriver(chromeOptions);
+    }
+
     public void Dispose()
     {
-        throw new NotImplementedException();
+        Quit();
+        GC.SuppressFinalize(this);
+    }
+    public void Quit()
+    {
+        if (!_disposed)
+        {
+            _driver?.Quit();
+            _disposed = true;   
+        }
+    }
+    ~WebScraper() 
+    {
+        Quit();
     }
 }
