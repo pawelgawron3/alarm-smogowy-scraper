4c65e42f279b69f3d70aec626be19225958b3d2a
pawelgawron3 <148467604+pawelgawron3@users.noreply.github.com>
2025-07-23 14:14:50 +0200

    Update WebScraper.cs, add NuGet package

--- AlarmSmogowySolution/ScraperConsole/WebScrapers/WebScraper.cs
+++ AlarmSmogowySolution/ScraperConsole/WebScrapers/WebScraper.cs
@@ -1,8 +1,14 @@
-﻿using OpenQA.Selenium.Chrome;
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using ScraperConsole.Models;
+using SeleniumExtras.WaitHelpers;
 
 namespace ScraperConsole.WebScrapers;
+
 public class WebScraper : IDisposable
 {
+    public const string url = "https://iee.org.pl";
     private ChromeDriver _driver;
     private bool _disposed = false;
 
@@ -19,20 +25,57 @@ public class WebScraper : IDisposable
         _driver = new ChromeDriver(chromeOptions);
     }
 
+    public List<Article> StartScraping(string url)
+    {
+        const int timeoutInterval = 5;
+        var articles = new List<Article>();
+
+        _driver.Navigate().GoToUrl(url);
+        var wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(timeoutInterval));
+
+        var link = wait.Until(ExpectedConditions.ElementToBeClickable(By.CssSelector("a[href='https://iee.org.pl/index.php/aktualnosci/']")));
+        link.Click();
+
+        var articlecontainers = _driver.FindElements(By.CssSelector("div.row-bg.viewport-desktop"));
+
+        foreach (var container in articlecontainers)
+        {
+            try
+            {
+                string title = container.FindElement(By.TagName("h4")).Text.Trim();
+                string content = container.FindElement(By.TagName("p")).Text.Trim();
+
+                Article article = new Article()
+                {
+                    Title = title,
+                    Content = content
+                };
+                articles.Add(article);
+            }
+            catch (NoSuchElementException)
+            {
+                continue;
+            }
+        }
+        return articles;
+    }   
+
     public void Dispose()
     {
         Quit();
         GC.SuppressFinalize(this);
     }
+
     public void Quit()
     {
         if (!_disposed)
         {
             _driver?.Quit();
-            _disposed = true;   
+            _disposed = true;
         }
     }
-    ~WebScraper() 
+
+    ~WebScraper()
     {
         Quit();
     }
