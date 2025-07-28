4e07f63f2a9d66e1fbfc01beff95a1cb5aa227bc
pawelgawron3 <148467604+pawelgawron3@users.noreply.github.com>
2025-07-28 12:38:02 +0200

    Refactor the scraper class
    Implemented the scraper base class

--- AlarmSmogowySolution/ScraperConsole/WebScrapers/WebScraper.cs
+++ AlarmSmogowySolution/ScraperConsole/WebScrapers/WebScraper.cs
@@ -1,34 +1,21 @@
 ﻿using System.Text.Json;
 using OpenQA.Selenium;
-using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Support.UI;
 using ScraperConsole.Models;
 using SeleniumExtras.WaitHelpers;
 
 namespace ScraperConsole.WebScrapers;
 
-public class WebScraper : IDisposable
+public class WebScraper : ScraperBaseClass
 {
-    private readonly string jsonPath = Path.Combine("Resources", "scrapingTargets.json");
     private string jsonString;
-    private ChromeDriver _driver;
-    private bool _disposed = false;
 
     public WebScraper()
     {
-        var chromeOptions = new ChromeOptions();
-
-        chromeOptions.AddArguments("--headless"); //without browser GUI
-        chromeOptions.AddArguments("--disable-gpu"); //for stability on Windows/Linux
-        chromeOptions.AddArguments("--disk-cache-size=0"); //always load the new page
-        chromeOptions.AddArguments("--no-sandbox"); //for Linux env
-        chromeOptions.AddArguments("--window-size=1920,1080"); //set the tab resolution
-
-        _driver = new ChromeDriver(chromeOptions);
         jsonString = File.ReadAllText(jsonPath);
     }
 
-    public List<Article> StartScraping(string url)
+    public override List<Article> StartScraping(string url)
     {
         const int timeoutInterval = 5;
         var articles = new List<Article>();
@@ -83,25 +70,5 @@ public class WebScraper : IDisposable
         }
 
         return articles;
-    }
-
-    public void Dispose()
-    {
-        Quit();
-        GC.SuppressFinalize(this);
-    }
-
-    public void Quit()
-    {
-        if (!_disposed)
-        {
-            _driver?.Quit();
-            _disposed = true;
-        }
-    }
-
-    ~WebScraper()
-    {
-        Quit();
     }
 }
