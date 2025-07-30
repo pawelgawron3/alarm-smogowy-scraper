c83962979ded8ee18a739d082b3b82509d625fad
pawelgawron3 <148467604+pawelgawron3@users.noreply.github.com>
2025-07-30 11:07:11 +0200

    Create ScraperController.cs

---
+++ AlarmSmogowySolution/ScraperConsole/Controllers/ScraperController.cs
@@ -0,0 +1,32 @@
+﻿using ScraperConsole.Models;
+using ScraperConsole.WebScrapers;
+
+namespace ScraperConsole.Controllers;
+public class ScraperController
+{
+    private readonly Dictionary<string, Func<IScraper>> _scraperFactory; 
+
+    public ScraperController()
+    {
+        _scraperFactory = new Dictionary<string, Func<IScraper>>()
+        {
+            { "https://iee.org.pl", () => new WebScraper()},
+        };
+    }
+
+    public List<Article> Scrape(string url)
+    {
+        if (!_scraperFactory.TryGetValue(url, out var scraperFactory))
+        {
+            Console.WriteLine($"No scraper available for domain: {url}");
+            return new List<Article>();
+        }
+        else
+        {
+            using (var scraper = scraperFactory())
+            {
+                return scraper.StartScraping(url);
+            }
+        }
+    } 
+}
