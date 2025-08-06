30e0457b1585e0fd32f34e60f83f0ad9bb91f2bf
pawelgawron3 <148467604+pawelgawron3@users.noreply.github.com>
2025-08-06 12:34:19 +0200

    Update PdfHelper.cs
    The code has been refactored, now there are tables handling/formatting.

--- AlarmSmogowySolution/ScraperConsole/Helpers/PdfHelper.cs
+++ AlarmSmogowySolution/ScraperConsole/Helpers/PdfHelper.cs
@@ -1,5 +1,6 @@
 ﻿using System.Text.RegularExpressions;
 using QuestPDF.Fluent;
+using QuestPDF.Helpers;
 using ScraperConsole.Models;
 
 namespace ScraperConsole.Helpers;
@@ -44,21 +45,85 @@ public static class PdfHelper
                             .Column(col =>
                             {
                                 col.Item().Text(article.Title)
-                                    .FontSize(18)
+                                    .FontSize(24)
                                     .FontFamily("Times New Roman")
                                     .SemiBold();
 
                                 col.Item().PaddingBottom(10);
 
-                                var lines = article.Content.Split('\n');
-
-                                foreach (var line in lines)
+                                foreach (var el in article.Elements)
                                 {
-                                    col.Item().Text(line)
-                                        .FontSize(12)
-                                        .FontFamily("Times New Roman")
-                                        .LineHeight(1.5f);
+                                    switch (el.ElementType)
+                                    {
+                                        case ArticleElementType.Paragraph:
+                                            col.Item().Text(el.Text)
+                                            .FontSize(12)
+                                            .LineHeight(1.5f);
+                                            col.Item().PaddingBottom(5);
+                                            break;
+
+                                        case ArticleElementType.Header:
+                                            col.Item().Text(el.Text)
+                                            .FontSize(16)
+                                            .Bold();
+                                            col.Item().PaddingBottom(5);
+                                            break;
+
+                                        case ArticleElementType.List:
+                                            foreach (var item in el.ListItems!)
+                                            {
+                                                col.Item().Text($"{item}")
+                                                .FontSize(12)
+                                                .LineHeight(1.5f);
+                                            }
+                                            col.Item().PaddingBottom(5);
+                                            break;
+
+                                        case ArticleElementType.Table:
+                                            if (el.TableData != null)
+                                            {
+                                                col.Item().Table(table =>
+                                                {
+                                                    int colCount = el.TableData.Max(r => r.Count);
+
+                                                    table.ColumnsDefinition(columns =>
+                                                    {
+                                                        for (int i = 0; i < colCount; i++)
+                                                        {
+                                                            columns.RelativeColumn();
+                                                        }
+                                                    });
+
+                                                    foreach (var row in el.TableData)
+                                                    {
+                                                        foreach (var cell in row)
+                                                        {
+                                                            table.Cell()
+                                                            .Border(0.5f)
+                                                            .BorderColor(Colors.Grey.Lighten1)
+                                                            .AlignCenter()
+                                                            .AlignMiddle()
+                                                            .Padding(0)
+                                                            .Text(cell)
+                                                            .FontSize(10);
+                                                        }
+                                                    }
+                                                });
+                                                col.Item().PaddingBottom(10);
+                                            }
+                                            break;
+                                    }
                                 }
+
+                                //var lines = article.Content.Split('\n');
+
+                                //foreach (var line in lines)
+                                //{
+                                //    col.Item().Text(line)
+                                //        .FontSize(12)
+                                //        .FontFamily("Times New Roman")
+                                //        .LineHeight(1.5f);
+                                //}
                             });
                     });
                 });
