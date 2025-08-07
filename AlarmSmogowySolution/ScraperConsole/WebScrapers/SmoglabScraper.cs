844cd77765631066627aadb3525e950039ddaad9
pawelgawron3 <148467604+pawelgawron3@users.noreply.github.com>
2025-08-07 10:34:21 +0200

    Create SmoglabScraper.cs

---
+++ AlarmSmogowySolution/ScraperConsole/WebScrapers/SmoglabScraper.cs
@@ -0,0 +1,92 @@
+﻿using System.Text.Json;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using ScraperConsole.Models;
+using SeleniumExtras.WaitHelpers;
+
+namespace ScraperConsole.WebScrapers;
+
+public class SmoglabScraper : ScraperBaseClass
+{
+    private string jsonString;
+
+    public SmoglabScraper()
+    {
+        jsonString = File.ReadAllText(jsonPath);
+    }
+
+    public override List<Article> StartScraping(string url)
+    {
+        const int timeoutInterval = 5;
+        var articles = new List<Article>();
+
+        var targets = JsonSerializer.Deserialize<Dictionary<string, List<string>>>(jsonString);
+        if (targets == null || !targets.ContainsKey(url))
+        {
+            Console.WriteLine($"There is no such page in .json file: {url}");
+            return articles;
+        }
+
+        var subpages = targets[url];
+        var wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(timeoutInterval));
+
+        foreach (var subpageUrl in subpages)
+        {
+            try
+            {
+                _driver.Navigate().GoToUrl(subpageUrl);
+
+                wait.Until(ExpectedConditions.ElementExists(By.CssSelector("h1.elementor-heading-title")));
+                wait.Until(ExpectedConditions.ElementExists(By.CssSelector("div.elementor-widget-container")));
+
+                string title = _driver.FindElement(By.CssSelector("h1.elementor-heading-title")).Text.Trim();
+
+                var articleContainer = _driver.FindElement(
+                    By.XPath("//div[contains(@class, 'elementor-widget-container')]" +
+                    "[.//p]" +
+                    "[.//h2]"));
+                var childElements = articleContainer.FindElements(By.XPath("./*"));
+
+                Article article = new Article()
+                {
+                    Title = title
+                };
+
+                foreach (var element in childElements)
+                {
+                    switch (element.TagName.ToLower())
+                    {
+                        case "p":
+                            article.Elements.Add(new ArticleElement
+                            {
+                                ElementType = ArticleElementType.Paragraph,
+                                Text = element.Text.Trim()
+                            });
+                            break;
+
+                        case "h2":
+                            article.Elements.Add(new ArticleElement
+                            {
+                                ElementType = ArticleElementType.Header,
+                                Text = element.Text.Trim()
+                            });
+                            break;
+                    }
+                }
+
+                articles.Add(article);
+            }
+            catch (NoSuchElementException ex)
+            {
+                Console.WriteLine($"The expected elements were not found on the page: {subpageUrl}\n{ex.Message}");
+                continue;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occured: {ex.Message}");
+            }
+        }
+
+        return articles;
+    }
+}
