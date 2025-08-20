fe3b0fdf9d7a8415a249351d2608ffdddc0f2b0e
pawelgawron3 <148467604+pawelgawron3@users.noreply.github.com>
2025-08-20 11:49:11 +0200

    Update HtmlListerHelper.cs for ol elements
    Now ol elements are also handled.

--- AlarmSmogowySolution/ScraperConsole/Helpers/HtmlListHelper.cs
+++ AlarmSmogowySolution/ScraperConsole/Helpers/HtmlListHelper.cs
@@ -5,19 +5,29 @@ namespace ScraperConsole.Helpers;
 
 public static class HtmlListHelper
 {
-    public static List<string> ExtractListItems(IWebElement ulElement, int level = 0)
+    public static List<string> ExtractListItems(IWebElement listElement, int level = 0)
     {
         List<string> items = new List<string>();
-        var liElements = ulElement.FindElements(By.XPath("./li"));
+        bool isOrdered = false;
+        int index = 1;
+
+        string tagName = listElement.TagName.ToLower();
+        if (tagName == "ol")
+        {
+            isOrdered = true;
+        }
+
+        var liElements = listElement.FindElements(By.XPath("./li"));
+
 
         foreach (var li in liElements)
         {
             string liText = li.Text.Trim();
 
-            var nestedUl = li.FindElements(By.XPath("./ul")).FirstOrDefault();
-            if (nestedUl != null)
+            var nestedList = li.FindElements(By.XPath("./ul | ./ol")).FirstOrDefault();
+            if (nestedList != null)
             {
-                string nestedText = nestedUl.Text.Trim();
+                string nestedText = nestedList.Text.Trim();
                 if (!string.IsNullOrEmpty(nestedText))
                 {
                     liText = liText.Replace(nestedText, "").Trim();
@@ -26,14 +36,25 @@ public static class HtmlListHelper
 
             if (!string.IsNullOrWhiteSpace(liText))
             {
-                string prefix = new string(' ', level * 5) + "- ";
+                string prefix;
+                if (isOrdered)
+                {
+                    prefix = new string(' ', level * 5) + $"{index}. ";
+                }
+                else
+                {
+                    prefix = new string(' ', level * 5) + "- ";
+                }
+
                 items.Add(prefix + Regex.Replace(liText, @"\[\d+\]", ""));
             }
 
-            if (nestedUl != null)
+            if (nestedList != null)
             {
-                items.AddRange(ExtractListItems(nestedUl, level + 1));
+                items.AddRange(ExtractListItems(nestedList, level + 1));
             }
+
+            index++;
         }
 
         return items;
